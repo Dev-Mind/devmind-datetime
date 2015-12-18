d3f81ad7a34fac88c5761a40c8c7ac18b4522769
Guillaume EHRET <guillaume@dev-mind.fr>
2015-12-18 10:05:01 +0100

    Fix test

--- src/main/java/com/devmind/date/ServiceJava8.java
+++ src/main/java/com/devmind/date/ServiceJava8.java
@@ -1,9 +1,6 @@
 package com.devmind.date;
 
-import java.time.Duration;
-import java.time.Instant;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.*;
 
 /**
  * @author Dev-Mind <guillaume@dev-mind.fr>
@@ -12,11 +9,26 @@ import java.time.LocalDateTime;
 public class ServiceJava8 {
 
     private Instant eventDate  = Instant.parse("2016-04-21T08:30:00.00Z");
+    private Clock clock = Clock.systemUTC();
 
     public String getCountDownBeforeEvent(){
         if(Instant.now().compareTo(eventDate)<0){
             return String.format("%s days before our event", Duration.between(Instant.now(), eventDate).toDays());
         }
         return String.format("Event closed since %s days", Duration.between(eventDate, Instant.now()).toDays());
+    }
+
+    public String getCountDownBeforeEvent2(){
+        if(Instant.now(clock).compareTo(eventDate)<0){
+            return String.format("%s days before our event", Duration.between(Instant.now(clock), eventDate).toDays());
+        }
+        return String.format("Event closed since %s days", Duration.between(eventDate, Instant.now(clock)).toDays());
+    }
+
+    /**
+     * Visible for test
+     */
+    public void setClock(Clock clock) {
+        this.clock = clock;
     }
 }
--- src/test/java/date/ServiceJava8Test.java
+++ src/test/java/date/ServiceJava8Test.java
@@ -2,7 +2,13 @@ package date;
 
 import com.devmind.date.ServiceJava8;
 import org.assertj.core.api.Assertions;
+import org.joda.time.DateTimeUtils;
+import org.junit.After;
 import org.junit.Test;
+
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneId;
 
 /**
  * @author Dev-Mind <guillaume@dev-mind.fr>
@@ -12,30 +18,21 @@ public class ServiceJava8Test {
 
     private ServiceJava8 service = new ServiceJava8();
 
-    @Test
-    public void should(){
-        Assertions.assertThat(service.getCountDownBeforeEvent()).isEqualTo("eze");
+    @After
+    public void tearDown(){
+        service.setClock(Clock.systemUTC());
     }
 
-//    public class DateTimeUtils {
-//        private static final DateTimeUtils.SystemMillisProvider SYSTEM_MILLIS_PROVIDER = new DateTimeUtils.SystemMillisProvider();
-//        private static volatile DateTimeUtils.MillisProvider cMillisProvider;
-//
-//        protected DateTimeUtils() {
-//        }
-//
-//        public static final long currentTimeMillis() {
-//            return cMillisProvider.getMillis();
-//        }
-//
-//        public static final void setCurrentMillisSystem() throws SecurityException {
-//            checkPermission();
-//            cMillisProvider = SYSTEM_MILLIS_PROVIDER;
-//        }
-//
-//        public static final void setCurrentMillisFixed(long var0) throws SecurityException {
-//            checkPermission();
-//            cMillisProvider = new DateTimeUtils.FixedMillisProvider(var0);
-//        }
+    @Test
+    public void should_test_a_date_before_event() {
+        service.setClock(Clock.fixed(Instant.parse("2016-04-18T00:00:00.00Z"), ZoneId.systemDefault()));
+        Assertions.assertThat(service.getCountDownBeforeEvent2()).isEqualTo("3 days before our event");
+    }
+
+    @Test
+    public void should_test_a_date_after_event() {
+        service.setClock(Clock.fixed(Instant.parse("2016-04-30T00:00:00.00Z"), ZoneId.systemDefault()));
+        Assertions.assertThat(service.getCountDownBeforeEvent2()).isEqualTo("Event closed since 8 days");
+    }
 
 }
